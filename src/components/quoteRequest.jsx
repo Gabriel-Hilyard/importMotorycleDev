d185975c9803fc65605dc0b5532d9ff9f982489b
b4gabe <gbhilyard@yahoo.com>
2025-04-10 19:58:19 -0700

    added email form to request a quote, need to finish account setup.

--- src/components/quoteRequest.jsx
+++ src/components/quoteRequest.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
-
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 function QuoteRequest() {
-  console.log("Hello, Quote Request is working")
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_ic93d7x', 'template_qke7oft', form.current, {
+        publicKey: '9YT3bAimNy4PYzfIr',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        }
+      );
+  };
+
   return (
-    <div>
-        
-    </div>
-  )
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
 }
 
 export default QuoteRequest;
